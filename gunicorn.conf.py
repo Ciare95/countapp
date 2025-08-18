32e93da046cabe2cad0d4ca6589a13a988a6ab15
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-08-18 14:12:00 -0500

    Error en gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,9 @@
-workers = 4
-bind = "0.0.0.0:5000"
-timeout = 30
+import os
+
+# Gunicorn configuration file
+
+# Server socket
+bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
+
+# Worker processes
+workers = int(os.environ.get('WEB_CONCURRENCY', 3))
